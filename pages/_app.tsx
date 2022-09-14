669e02437a46bd525767f663ef71460b716aed37
Hasan Joldic <hasan.joldic@enki.ba>
2022-09-14 17:36:48 +0200

    chore: test cookies on subdomains

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,6 +42,8 @@ const App = ({ Component, pageProps, paletteMode }: Props) => {
     router.push(path);
   };
 
+  console.log("App", paletteMode);
+
   return (
     <ThemeProvider paletteMode={paletteMode}>
       <Head>
@@ -63,8 +65,8 @@ const App = ({ Component, pageProps, paletteMode }: Props) => {
 };
 
 App.getInitialProps = async (context: AppContext) => {
-  const paletteMode =
-    getCookie("paletteMode", context.ctx.req?.headers.cookie) || "light";
+  console.log("App.getInitialProps", context.ctx.req?.headers.cookie);
+  const paletteMode = getCookie("paletteMode", context.ctx.req?.headers.cookie);
 
   return { paletteMode } as unknown as Props;
 };
