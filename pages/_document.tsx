27aed5ce2b3c3ac5cfaf1dc0337e1e544e97ebb8
Hasan Joldic <office@hasanjoldic.com>
2023-02-28 16:05:02 +0100

    move hotjar script to script file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <!-- Hotjar Tracking Code for my site --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3386986,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        ></script>
+        <Script type="text/javascript" src="/hotjar/script.js" />
       </Head>
       <body>
         <Main />
